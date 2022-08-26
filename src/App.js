950db599d3885027bb98ff9cb8808a249a77bf1c
genmancoder <genmansystems@gmail.com>
2022-08-26 08:50:18 +0800

    Added another image

--- src/App.js
+++ src/App.js
@@ -86,13 +86,13 @@ function App() {
             <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">          
                 
                 
-                <div class="mt-4 md:mt-0">
+                <div className="mt-4 md:mt-0 flex justify-center">
                     <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-green-400">John Ray D. Paulin</h2>
                     <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Frontend Developer</p>
-                    <a href="#" class="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
-                        Get started
-                        <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                    </a>
+                    
+                </div>
+                <div className='mt-4 md:mt-0'>
+                        <img src='https://raw.githubusercontent.com/genmancoder/portfolio/staging/src/assets/dark.png' alt='profile' />
                 </div>
             </div>
             </section>
